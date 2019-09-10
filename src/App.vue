f9648b11113e7b43f29c579d0b7648ec44ea5459
Irene Chan <irene.chan.16@outlook.com>
2019-09-10 17:38:12 +0800

    Setting up vue-app for Har

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<!--       <router-link to="/">Home</router-link> |
+      <router-link to="/about">ddddddAbout</router-link> -->
+  <div id="app">
+    <side-bar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import SideBar from '@/components/SideBar.vue'
+  export default {
+    name: 'app',
+      components: {
+        SideBar,
+      }
+    }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Niramit:700|Roboto|Staatliches&display=swap');
+
+#app {
+  font-family: "Niramit", sans-serif, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: none;
+}
+
+
+/*#nav a.router-link-exact-active {
+  color: #42b983;
+}*/
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="nav">
+      <ul>
+        <li id="nav-tabs">
+          <img alt="bento Go!" src="@/assets/bento-menu.svg" style="width: 25px" />
+        </li>
+        <div id="social-media">
+          <li>
+            <i class="fab fa-youtube"></i>
+          </li>
+          <li>
+            <i class="fab fa-instagram"></i>
+          </li>
+          <li>
+          <i class="fab fa-facebook-f"></i>
+        </li>
+        </div>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SideBar"
+    }
+</script>
+
+<style>
+
+</style>
