f4bf8b5bf5e92aae335a70dd5a4cb9d80a372ed5
Irene Chan <irene.chan.16@outlook.com>
2019-09-11 13:14:48 +0800

    Adding menu toggle & transitioning when enter new view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="all">
-    <side-bar />
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">ddddddAbout</router-link>
+    <side-bar @isHidden />
     <div id="app">
-      <router-view />
+      <transition name="moveInUp">
+       <router-view />
+      </transition>
     </div>
   </div>
 </template>
@@ -13,10 +13,13 @@
   import SideBar from '@/components/SideBar.vue'
   export default {
     name: 'app',
-      components: {
-        SideBar,
-      }
+    components: {
+      SideBar,
+    },
+    method: {
+
     }
+  }
 </script>
 
 <style>
@@ -24,16 +27,40 @@
 
 #app {
   font-family: "Niramit", sans-serif, Arial, sans-serif;
-
-
   text-align: center;
-
 }
 
 ul {
   list-style-type: none;
 }
 
+.moveInUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
 
+@keyframes fadeIn {
+  0%  {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+.moveInUp-leave-active {
+  animation: moveInUp .3s ease-in;
+}
+
+@keyframes moveInUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
+}
 
 </style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="sidebar">
         <div class="nav">
-            <div class="nav-menu">
-                <img alt="bento Go!" src="@/assets/bento-menu.svg" style="width: 25px" />
+            <div class="nav-menu" >
+                <img class="menu-toggle" v-on:click="isHidden = !isHidden" alt="bento Go!" src="@/assets/bento-menu.svg" style="width: 25px" />
+                <div class="menu-list" v-if="!isHidden">
+                    <router-link class="menu-item" to="/">Home</router-link>
+                    <router-link class="menu-item" to="/about">About Us</router-link>
+                    <router-link class="menu-item" to="/FAQ">FAQ</router-link>
+                </div>
             </div>
             <div class="nav-logo">Har Studio.</div>
             <div class="nav-menu">
@@ -16,7 +21,12 @@
 
 <script>
     export default {
-        name: "SideBar"
+        name: "SideBar",
+        data() {
+            return {
+                isHidden: true
+            };
+        },
     }
 </script>
 
@@ -50,6 +60,32 @@
         transform: rotate(-90deg);
         white-space: nowrap;
     }
+
+    .menu-list {
+        font-size: 15px;
+        white-space: nowrap;
+        position: absolute;
+        margin: 0px;
+        top: 80px;
+        left: 30px;
+
+    }
+
+    .menu-list * {
+        /*padding-top: 10px;*/
+
+    }
+
+    .menu-list a {
+        text-decoration: none;
+        color: grey;
+    }
+
+    .menu-list a.router-link-exact-active {
+        text-decoration: none;
+        color: black;
+    }
+
 
 
 </style>
